a0336c7436a1eb99218800bb9d6b640f2f07a6c4
PANXiao1994 <panxiao1994@gmail.com>
2018-01-23 17:20:35 +0100

    aaa

--- problem.py
+++ problem.py
@@ -6,8 +6,10 @@ import rampwf as rw
 from datetime import timedelta
 
 problem_title = 'personality prediction'
-_target_column_name = 'personality'
+_target_column_name = 'type'
 _prediction_label_names = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+dict = {'INFJ':0, 'ENTP':1, 'INTP':2, 'INTJ':3, 'ENTJ':4, 'ENFJ':5, 'INFP':6, 'ENFP':7, 'ISFP':8, 'ISTP':9,
+ 'ISFJ':10, 'ISTJ':11, 'ESTP':12, 'ESFP':13, 'ESTJ':14, 'ESFJ':15}
 # A type (class) which will be used to create wrapper objects for y_pred
 Predictions = rw.prediction_types.make_multiclass(
     label_names=_prediction_label_names)
@@ -45,17 +47,18 @@ def get_cv(X, y):
     for i in range(n_splits):
         test = arr[int(i*fold_length):int((i+1)*fold_length)]
         train = np.concatenate((arr[:int(i*fold_length)],arr[int((i+1)*fold_length):]))
-        print(arr[train], arr[test])
+        yield(arr[train], arr[test])
         
 def _read_data(path, f_name):
-    data = pd.read_csv(os.path.join(path, 'data', f_name), sep='\t')
-    y_array = data[_target_column_name].values
+    data = pd.read_csv(os.path.join(path, 'data', f_name), sep=',')
+    y_array1 = data[_target_column_name].values
+    y_array = np.array([dict[k] for k in y_array1])
     X_df = data.drop(_target_column_name, axis=1)
     test = os.getenv('RAMP_TEST_MODE', 0)
     if test:
         return X_df[:100], y_array[:100]
     else:
-        return X_df, y_array
+        return X_df[:1000], y_array[:1000]
 
 
 def get_train_data(path='.'):
