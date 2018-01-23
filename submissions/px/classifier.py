ec5a3d7e726eec7b3934577db7ab6a7f327ed7ba
PANXiao1994 <panxiao1994@gmail.com>
2018-01-23 16:48:13 +0100

    1

--- submissions/px/classifier.py
+++ submissions/px/classifier.py
@@ -1,43 +1,17 @@
 # -*- coding: utf-8 -*-
 from sklearn.base import BaseEstimator
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.decomposition import LatentDirichletAllocation, PCA
-from sklearn.neural_network import MLPClassifier
-from sklearn.svm import SVC
-import xgboost as xgb
-import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+
 
 class Classifier(BaseEstimator):
     def __init__(self):
-        self.clf = xgb.XGBRegressor(max_depth=10, learning_rate=0.5, n_estimators=100, 
-                                     silent=True, booster='gbtree',
-                                     eval_metric='auc',
-                                     n_jobs=1, nthread=None, gamma=0, min_child_weight=1, 
-                                     max_delta_step=0, subsample=1, colsample_bytree=1, 
-                                     colsample_bylevel=1, reg_alpha=0, reg_lambda=1, 
-                                     scale_pos_weight=1, base_score=0.5, random_state=0, 
-                                     seed=None, missing=None)
+        self.clf = RandomForestClassifier()
 
     def fit(self, X, y):
-        self.clf.fit(X, list(map(int, y)))
+        self.clf.fit(X.todense(), y)
 
     def predict(self, X):
-        pred = self.clf.predict(X)
-        result = np.zeros((X.shape[0], 6))
-        for i in range(X.shape[0]):
-            idx = int(round(pred[i]))
-            if(idx > 5):
-                idx = 5
-            result[i][idx] = 1
-        return result
+        return self.clf.predict(X.todense())
 
     def predict_proba(self, X):
-        pred = self.clf.predict(X)
-        result = np.zeros((X.shape[0], 6))
-        for i in range(X.shape[0]):
-            idx = int(round(pred[i]))
-            if(idx > 5):
-                idx = 5
-            result[i][idx] = 1
-        return result
+        return self.clf.predict_proba(X)
--- submissions/px/feature_extractor.py
+++ submissions/px/feature_extractor.py
@@ -3,31 +3,31 @@
 from __future__ import unicode_literals
 
 from sklearn.feature_extraction.text import TfidfVectorizer
-import string
-import unicodedata
-import numpy as np
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-from nltk.stem import SnowballStemmer
-from sklearn.utils.validation import check_is_fitted
-from sklearn.preprocessing import OneHotEncoder, MaxAbsScaler
-from sklearn.decomposition import LatentDirichletAllocation
 
-# def document_preprocessor(doc):
-#     # TODO: is there a way to avoid these encode/decode calls?
-#     try:
-#         doc = unicode(doc, 'utf-8')
-#     except NameError:  # unicode is a default on python 3
-#         pass
-#     doc = unicodedata.normalize('NFD', doc)
-#     doc = doc.encode('ascii', 'ignore')
-#     doc = doc.decode("utf-8")
-#     return str(doc)
+
+def document_preprocessor(doc):
+    """ A custom document preprocessor
+
+    This function can be edited to add some additional
+    transformation on the documents prior to tokenization.
+
+    At present, this function passes the document through
+    without modification.
+    """
+    return doc
+
 
 def token_processor(tokens):
-    stemmer = SnowballStemmer('english')
-    for token in tokens:
-        yield stemmer.stem(token)
+    """ A custom token processor
+
+    This function can be edited to add some additional
+    transformation on the extracted tokens (e.g. stemming)
+
+    At present, this function just passes the tokens through.
+    """
+    for t in tokens:
+        yield t
+
 
 class FeatureExtractor(TfidfVectorizer):
     """Convert a collection of raw docs to a matrix of TF-IDF features. """
@@ -36,14 +36,7 @@ class FeatureExtractor(TfidfVectorizer):
         # see ``TfidfVectorizer`` documentation for other feature
         # extraction parameters.
         super(FeatureExtractor, self).__init__(
-                analyzer='word',ngram_range = (1,2),
-                stop_words='english', decode_error='replace',
-                strip_accents='unicode',
-                max_df = 1.0, min_df = 0.0001)
-        self.lda = LatentDirichletAllocation(n_components=7, max_iter=20,
-                                learning_method='online',
-                                learning_offset=50.,
-                                random_state=None)
+                analyzer='word', preprocessor=document_preprocessor)
 
     def fit(self, X_df, y=None):
         """Learn a vocabulary dictionary of all tokens in the raw documents.
@@ -54,21 +47,16 @@ class FeatureExtractor(TfidfVectorizer):
             a DataFrame, where the text data is stored in the ``statement``
             column.
         """
-        X_tf = super(FeatureExtractor, self).fit_transform(X_df.statement)
-        self.lda.fit(X_tf)
+        super(FeatureExtractor, self).fit(X_df.statement)
         return self
 
     def fit_transform(self, X_df, y=None):
-        
-        super(FeatureExtractor, self).fit(X_df)
-        
-        X_tf = self.transform(X_df)
-        return self.lda.transform(X_tf)
+        return self.fit(X_df).transform(X_df)
 
     def transform(self, X_df):
         X = super(FeatureExtractor, self).transform(X_df.statement)
-        return self.lda.transform(X)
-    
+        return X
+
     def build_tokenizer(self):
         """
         Internal function, needed to plug-in the token processor, cf.
