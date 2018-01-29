72d99a8488d686e9f64c17003670169653f12a56
PANXiao1994 <panxiao1994@gmail.com>
2018-01-29 22:32:11 +0100

    p

--- submissions/wyz/classifier.py
+++ submissions/wyz/classifier.py
@@ -2,12 +2,14 @@
 from sklearn.base import BaseEstimator
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
 
 
 class Classifier(BaseEstimator):
     def __init__(self):
         #self.clf = RandomForestClassifier()
-        self.clf = SVC(probability=True,class_weight='balanced',kernel='linear')
+        #self.clf = SVC(probability=True,class_weight='balanced',kernel='linear')
+        self.clf = MLPClassifier()
 
     def fit(self, X, y):
         #print(y)
