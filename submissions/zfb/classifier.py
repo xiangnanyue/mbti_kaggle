7b18d0562247c8ac2da7ae62c6f67fd11aab105d
shuishenshui <wuyunzhi@outlook.com>
2018-01-30 19:15:26 +0100

    Update classifier.py

--- submissions/zfb/classifier.py
+++ submissions/zfb/classifier.py
@@ -1,23 +1,18 @@
 # -*- coding: utf-8 -*-
 from sklearn.base import BaseEstimator
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import SVC
+from xgboost import XGBClassifier
 
 
 class Classifier(BaseEstimator):
     def __init__(self):
-        #self.clf = RandomForestClassifier()
-        self.clf = SVC(probability=True,class_weight='balanced',kernel='linear')
+        self.clf = XGBClassifier(n_estimators=88, learning_rate=0.05)
 
     def fit(self, X, y):
-        #print(y)
         self.clf.fit(X, y)
 
     def predict(self, X):
         return self.clf.predict(X)
 
     def predict_proba(self, X):
-        #print(X.shape)
         y = self.clf.predict_proba(X)
-        #print(y.shape)
         return y
